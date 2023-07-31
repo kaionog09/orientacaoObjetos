367adeb4f3fe56e78d1a2c4ba067331057d04d18
kaio <1kaiosouz@gmail.com>
2023-07-31 20:36:24 -0300

    Inicio do controle de versão

---
+++ src/main/java/com/itb/exemplo/orientacaoObjetos/OrientacaoObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.itb.exemplo.orientacaoObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientacaoObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientacaoObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/exemplo/orientacaoObjetos/OrientacaoObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.exemplo.orientacaoObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientacaoObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
